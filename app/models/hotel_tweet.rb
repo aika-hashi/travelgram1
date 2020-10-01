028a979cbaefb2ad8afadf906827dd2d4710dedd
aika-hashi <yu_akiramenai542_0224@yahoo.co.jp>
2020-10-01 15:49:53 +0900

    バリデーション

--- app/models/hotel_tweet.rb
+++ app/models/hotel_tweet.rb
@@ -5,4 +5,7 @@ class HotelTweet < ApplicationRecord
   belongs_to_active_hash :crowd
   belongs_to_active_hash :day
   belongs_to_active_hash :week
+
+  validates :area_id, numericality: { other_than: 1 }
+  validates :images, :local, :title, :price,:hotel_text,presence: true
 end
--- app/models/trip_tweet.rb
+++ app/models/trip_tweet.rb
@@ -4,4 +4,7 @@ class TripTweet < ApplicationRecord
   belongs_to_active_hash :area
   belongs_to_active_hash :crowd
   belongs_to_active_hash :week
+
+  validates :area_id, numericality: { other_than: 1 }
+  validates :images,:local, :title, :price,:spot_text,presence: true
 end
