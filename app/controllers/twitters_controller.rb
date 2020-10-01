1c031f43a94be63ee06a19da48ad9153a1f50a08
aika-hashi <yu_akiramenai542_0224@yahoo.co.jp>
2020-10-01 17:13:32 +0900

    ルーティング、コントローラー記述

--- app/controllers/twitters_controller.rb
+++ app/controllers/twitters_controller.rb
@@ -1,2 +1,19 @@
 class TwittersController < ApplicationController
+  def new
+    @hoteltweet = TwitterHotelTweet.new   #「UserDonation」に編集
+  end
+ 
+  def create
+    @hoteltweet = TwitterHotelTweet.new(hoteltweet_params)   #「UserDonation」に編集
+    @hoteltweet.save
+  end
+ 
+  private
+ 
+  def hoteltweet_params
+   #「投稿者」のキーも追加
+   params.require(:twitter_hotel_tweet).permit(:user_id, :twitter_id,:price, :discount, :age_id, :area_id, :day_id,:week_id, :crowd_id, :title, :text, :corona_measure, :local, )
+  end
+ 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:edit, :update]
+
+  resources :twitters,only: [:index, :show]
+  resources :hotel_tweets
+  resources :trip_tweets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
